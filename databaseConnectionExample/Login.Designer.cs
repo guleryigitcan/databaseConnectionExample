b9acabf17deb01e82a49758494917032d000c554
Yiğitcan Güler <37241744+guleryigitcan@users.noreply.github.com>
2021-08-26 14:09:43 +0300

    customers and products table added

---
+++ databaseConnectionExample/Login.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace databaseConnectionExample
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_logIn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_dataLink = new System.Windows.Forms.TextBox();
+            this.label_signIn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Location = new System.Drawing.Point(130, 131);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(134, 20);
+            this.textBox_username.TabIndex = 0;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(130, 186);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(134, 20);
+            this.textBox_password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(47, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(44, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // button_logIn
+            // 
+            this.button_logIn.Location = new System.Drawing.Point(168, 274);
+            this.button_logIn.Name = "button_logIn";
+            this.button_logIn.Size = new System.Drawing.Size(75, 23);
+            this.button_logIn.TabIndex = 4;
+            this.button_logIn.Text = "Login";
+            this.button_logIn.UseVisualStyleBackColor = true;
+            this.button_logIn.Click += new System.EventHandler(this.button_logIn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Database Link:";
+            // 
+            // textBox_dataLink
+            // 
+            this.textBox_dataLink.Location = new System.Drawing.Point(130, 236);
+            this.textBox_dataLink.Name = "textBox_dataLink";
+            this.textBox_dataLink.Size = new System.Drawing.Size(134, 20);
+            this.textBox_dataLink.TabIndex = 7;
+            // 
+            // label_signIn
+            // 
+            this.label_signIn.AutoSize = true;
+            this.label_signIn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label_signIn.Location = new System.Drawing.Point(108, 279);
+            this.label_signIn.Name = "label_signIn";
+            this.label_signIn.Size = new System.Drawing.Size(40, 13);
+            this.label_signIn.TabIndex = 8;
+            this.label_signIn.Text = "Sign In";
+            this.label_signIn.Click += new System.EventHandler(this.label_signIn_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 450);
+            this.Controls.Add(this.label_signIn);
+            this.Controls.Add(this.textBox_dataLink);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_logIn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_username);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_logIn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_dataLink;
+        private System.Windows.Forms.Label label_signIn;
+    }
+}
---
+++ databaseConnectionExample/signIn.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace databaseConnectionExample
+{
+    partial class signIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.button_sign = new System.Windows.Forms.Button();
+            this.textBox_databaseLink = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "E-mail:";
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Location = new System.Drawing.Point(130, 66);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(138, 20);
+            this.textBox_email.TabIndex = 3;
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Location = new System.Drawing.Point(130, 99);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(138, 20);
+            this.textBox_username.TabIndex = 4;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(130, 130);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(138, 20);
+            this.textBox_password.TabIndex = 5;
+            // 
+            // button_sign
+            // 
+            this.button_sign.Location = new System.Drawing.Point(147, 207);
+            this.button_sign.Name = "button_sign";
+            this.button_sign.Size = new System.Drawing.Size(89, 23);
+            this.button_sign.TabIndex = 6;
+            this.button_sign.Text = "Sign In";
+            this.button_sign.UseVisualStyleBackColor = true;
+            this.button_sign.Click += new System.EventHandler(this.button_sign_Click);
+            // 
+            // textBox_databaseLink
+            // 
+            this.textBox_databaseLink.Location = new System.Drawing.Point(130, 165);
+            this.textBox_databaseLink.Name = "textBox_databaseLink";
+            this.textBox_databaseLink.Size = new System.Drawing.Size(138, 20);
+            this.textBox_databaseLink.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Database Link:";
+            // 
+            // signIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 343);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_databaseLink);
+            this.Controls.Add(this.button_sign);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.textBox_email);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "signIn";
+            this.Text = "signIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button button_sign;
+        private System.Windows.Forms.TextBox textBox_databaseLink;
+        private System.Windows.Forms.Label label4;
+    }
+}
